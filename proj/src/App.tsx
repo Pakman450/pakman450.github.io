447de28d5f470db2821f033148d81053ad152ea4
Pakman450 <steven.pak10@gmail.com>
2023-04-07 20:01:53 -0400

    feat: start blog

---
+++ proj/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Navigation from './Navigation'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <Navigation/>
+    <div className="App">
+
+
+    </div>
+  
+  </>
+
+  )
+}
+
+export default App
