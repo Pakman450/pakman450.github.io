7fa973b386f26cf5504e63f2c0c00b7b5e711811
Steven Pak <steven.pak10@gmail.com>
2023-04-10 21:41:20 -0400

    fix: simplify nav bar

--- proj/src/Navigation.tsx
+++ proj/src/Navigation.tsx
@@ -32,21 +32,34 @@ export default function Navigation() {
 
   return (
     <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
 
-        <a href="#" className="-m-1.5 p-1.5 flex-1">
-          <span className="sr-only">Your Company</span>
-          <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-        </a>
-        <a href="#" className="flex-1 text-sm font-semibold leading-6 text-gray-900">
-          Projects
-        </a>
-        <a href="#" className="flex-1 ext-sm font-semibold leading-6 text-gray-900">
-          Contact Me
-        </a>
-        <a href="#" className="flex-1 text-sm font-semibold leading-6 text-gray-900">
-          Current Work
-        </a>
+        <div className=''>
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </a>
+        </div>
+
+        <div className='w-96'> 
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Projects
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Contact Me
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Current Work
+          </a>
+
+        </div>
+
+        <div className=''>
+            <a href="#" className="flex-1 text-sm font-semibold leading-6 text-gray-900">
+              Contact Me  
+            </a>
+          </div>
+
 
         {/* <div className="flex-1 lg:hidden">
           <button
--- proj/tailwind.config.js
+++ proj/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        '2': '2 2 0%'
+      }
+    },
   },
   plugins: [],
 }
