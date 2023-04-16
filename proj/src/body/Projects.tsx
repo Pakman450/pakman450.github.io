08845dd40aa5bb4d8f9a3e19f490c42577faf51c
Steven Pak <steven.pak10@gmail.com>
2023-04-16 12:09:01 -0400

    feat: update project space

--- proj/src/body/Projects.tsx
+++ proj/src/body/Projects.tsx
@@ -3,14 +3,14 @@ function Projects() {
   return (
     <>
         <div className='py-20 w-full grid justify-center max-w-7xl rounded-lg my-2 md:flex text-white bg-[#19376D] drop-shadow-2xl'>
-            <div className=" snap-x flex snap-mandatory bg-white py-10  w-[20rem] sm:w-[30rem] md:w-[40rem]	lg:px-10 lg:w-full lg:max-w-4xl overflow-y-hidden">
-              <div className="snap-start bg-slate-100 scroll-ml-6  grid min-w-full h-80 place-items-center text-8xl">
+            <div className=" snap-x flex snap-mandatory bg-white rounded-lg  py-10  w-[20rem] sm:w-[30rem] md:w-[40rem]	lg:px-10 lg:w-full lg:max-w-4xl overflow-y-hidden">
+              <div className="snap-start bg-black scroll-ml-2 ml-2 mr-6 grid min-w-full	 h-80 place-items-center text-8xl">
                 1
               </div>
-              <div className="snap-start bg-orange-200 scroll-ml-6 grid min-w-full h-80 place-items-center text-8xl">
+              <div className="snap-start bg-orange-200 scroll-ml-2 mr-6 grid min-w-full h-80 place-items-center text-8xl">
                 2
               </div>
-              <div className="snap-start bg-green-200 scroll-ml-6  grid min-w-full h-80 place-items-center text-8xl">
+              <div className="snap-start bg-green-200 scroll-ml-2 mr-6 grid min-w-full h-80 place-items-center text-8xl">
                 3
               </div>
             </div>
