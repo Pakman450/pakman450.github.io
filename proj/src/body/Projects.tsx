247be3f6e9a52bcbfdb600f6c13752f807ecfbb8
Steven Pak <steven.pak10@gmail.com>
2023-04-16 10:48:56 -0400

    feat: made the snap x responsive

--- proj/src/body/Projects.tsx
+++ proj/src/body/Projects.tsx
@@ -1,23 +1,19 @@
-import React from 'react'
-
-
+// min-[375px]:w-[30rem] sm:w-[40rem] md:
 function Projects() {
   return (
     <>
-        <div className='bg-[#19376D] my-2 py-20  w-[80rem] max-w-7xl rounded-lg drop-shadow-md'>
-          <div className=' bg-white flex justify-center'>
-            <div className="scroll-pl-10 snap-x flex snap-mandatory h-96 w-96 mx:auto overflow-y-hidden">
-              <div className="snap-start bg-slate-100 scroll-ml-6  grid min-w-full h-screen place-items-center text-8xl">
+        <div className='py-20 w-full grid justify-center max-w-7xl rounded-lg my-2 md:flex text-white bg-[#19376D] drop-shadow-2xl'>
+            <div className=" snap-x flex snap-mandatory bg-white py-10  w-[20rem] sm:w-[30rem] md:w-[40rem]	lg:px-10 lg:w-full lg:max-w-4xl overflow-y-hidden">
+              <div className="snap-start bg-slate-100 scroll-ml-6  grid min-w-full h-80 place-items-center text-8xl">
                 1
               </div>
-              <div className="snap-start bg-orange-200 scroll-ml-6 grid min-w-full h-screen place-items-center text-8xl">
+              <div className="snap-start bg-orange-200 scroll-ml-6 grid min-w-full h-80 place-items-center text-8xl">
                 2
               </div>
-              <div className="snap-start bg-green-200 scroll-ml-6  grid min-w-full h-screen place-items-center text-8xl">
+              <div className="snap-start bg-green-200 scroll-ml-6  grid min-w-full h-80 place-items-center text-8xl">
                 3
               </div>
             </div>
-          </div>
         </div>
     </>
   )
