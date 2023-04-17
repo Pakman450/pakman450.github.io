2b85715c3ddf6a3d08aec82627370680c4f1edee
Steven Pak <steven.pak10@gmail.com>
2023-04-16 21:43:07 -0400

    feat: center each snap

--- proj/src/body/Projects.tsx
+++ proj/src/body/Projects.tsx
@@ -2,15 +2,17 @@
 function Projects() {
   return (
     <>
-        <div className='py-20 w-full grid justify-center max-w-7xl rounded-lg my-2 md:flex text-white bg-[#19376D] drop-shadow-2xl'>
-            <div className=" snap-x flex snap-mandatory bg-white rounded-lg  py-10  w-[20rem] sm:w-[30rem] md:w-[40rem]	lg:px-10 lg:w-full lg:max-w-4xl overflow-y-hidden">
-              <div className="snap-start bg-black scroll-ml-2 ml-2 mr-6 grid min-w-full	 h-80 place-items-center text-8xl">
-                1
+        <div className=' w-full grid justify-center max-w-7xl rounded-lg my-2 py-20 px-10 text-white bg-[#19376D] drop-shadow-2xl'>
+            {/* <div className=" snap-x flex snap-mandatory bg-white space-x-10 rounded-lg py-10 md:py-40  w-[20rem] sm:w-[30rem] md:w-[40rem]	lg:px-10 lg:w-full lg:max-w-4xl overflow-y-hidden"> */}
+            <div className=" snap-x flex snap-mandatory bg-white space-x-10 rounded-lg py-10 md:py-40 px-10 w-full max-w-4xl overflow-y-hidden">
+
+              <div className="snap-center bg-black  grid min-w-[90%] h-80 place-items-center text-8xl border-black rounded-2xl drop-shadow-2xl">
+                1asdfasdfsdf
               </div>
-              <div className="snap-start bg-orange-200 scroll-ml-2 mr-6 grid min-w-full h-80 place-items-center text-8xl">
+              <div className="snap-center bg-orange-200 grid min-w-[90%] h-80 place-items-center text-8xl">
                 2
               </div>
-              <div className="snap-start bg-green-200 scroll-ml-2 mr-6 grid min-w-full h-80 place-items-center text-8xl">
+              <div className="snap-center bg-green-200   grid min-w-[90%] h-80 place-items-center text-8xl">
                 3
               </div>
             </div>
