77ba266eadc0a4c14f4bb7bd95a9675119111f45
Steven Pak <steven.pak10@gmail.com>
2023-04-16 21:54:09 -0400

    feat: simplify project

--- proj/src/body/Projects.tsx
+++ proj/src/body/Projects.tsx
@@ -4,10 +4,10 @@ function Projects() {
     <>
         <div className=' w-full grid justify-center max-w-7xl rounded-lg my-2 py-20 px-10 text-white bg-[#19376D] drop-shadow-2xl'>
             {/* <div className=" snap-x flex snap-mandatory bg-white space-x-10 rounded-lg py-10 md:py-40  w-[20rem] sm:w-[30rem] md:w-[40rem]	lg:px-10 lg:w-full lg:max-w-4xl overflow-y-hidden"> */}
-            <div className=" snap-x flex snap-mandatory bg-white space-x-10 rounded-lg py-10 md:py-40 px-10 w-full max-w-4xl overflow-y-hidden">
+            <div className=" snap-x flex snap-mandatory bg-white space-x-10 rounded-lg p-10 min-w-full max-w-4xl overflow-y-hidden">
 
-              <div className="snap-center bg-black  grid min-w-[90%] h-80 place-items-center text-8xl border-black rounded-2xl drop-shadow-2xl">
-                1asdfasdfsdf
+              <div className="snap-center bg-black  grid min-w-[90%] h-80 place-items-center text-8xl">
+                1asasdfasdfsdfsd
               </div>
               <div className="snap-center bg-orange-200 grid min-w-[90%] h-80 place-items-center text-8xl">
                 2
