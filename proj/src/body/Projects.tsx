6792379c5c668bf39bf1f81891ddcefb651020fe
Steven Pak <steven.pak10@gmail.com>
2023-04-18 21:12:01 -0400

    feat: start filling each project card

--- proj/src/body/Projects.tsx
+++ proj/src/body/Projects.tsx
@@ -7,13 +7,28 @@ function Projects() {
             <div className=" snap-x flex snap-mandatory bg-white space-x-10 rounded-lg p-10 min-w-full max-w-4xl overflow-y-hidden">
 
               <div className="snap-center bg-black  grid min-w-[90%] h-80 place-items-center text-8xl">
-                1asasdfasdfsdfsd
+                <h2 className="text-xl">
+                  Synthesizing inhibtors for PARP proteins
+                </h2>
+                <h2 className="text-xl">
+                  Synthesizing inhibtors for PARP proteins
+                </h2>
               </div>
               <div className="snap-center bg-orange-200 grid min-w-[90%] h-80 place-items-center text-8xl">
-                2
+                <h2 className="text-xl">
+                  Measuring gluten levels in medications
+                </h2>
+                <h2 className="text-xl">
+                  Measuring gluten levels in medications
+                </h2>
               </div>
               <div className="snap-center bg-green-200   grid min-w-[90%] h-80 place-items-center text-8xl">
-                3
+                <h2 className="text-xl">
+                  Driving molecular construction via descriptors
+                </h2>
+                <h2 className="text-xl">
+                  Driving molecular construction via descriptors
+                </h2>
               </div>
             </div>
         </div>
