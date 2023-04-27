5c573bdd8509836fc04bb797e174ffeb7f691c17
Steven Pak <69532598+Pakman450@users.noreply.github.com>
2023-04-27 16:32:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pakman450.github.io/',
   plugins: [react()],
   assetsInclude: '**/*.glb',
 })
